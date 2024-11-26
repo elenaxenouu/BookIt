817d142bea7287fccdb2f41389a6dc206b1487bc
elenaxenouu <t8220110@aueb.gr>
2024-11-26 18:24:05 +0200

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -13,17 +13,20 @@ public class Menu {
         Scanner scanner = new Scanner(System.in);
         int choice;
         
-            System.out.println("Καλωσήρθατε στο σύστημα κρατήσεων!");
-            System.out.println("1. Είσοδος Διαχειριστή");
-            System.out.println("2. Είσοδος Πελάτη");
-            System.out.println("0. Έξοδος");
+        
+            System.out.println("Welcome to reservation system!");
+            System.out.println("1. Admin login");
+            System.out.println("2. Customer ");
+            System.out.println("0. Exit");
+            System.out.print("Choice: ");
             choice = scanner.nextInt();
-            // Ελεγχος εγκυροτητας στοιχειων εισοδου 
+            
             while (choice !=0 && choice !=1 && choice!=2) {
-           System.out.println("Η επιλογή σας δεν είναι έγκυρη. Προσπαθήστε ξανά.");
-           System.out.println("1. Είσοδος Διαχεριστή");
-           System.out.println("2. Είσοδος Πελάτη ");
-           System.out.println("0. Έξοδος");
+           System.out.println("Not valid answer.Please try again.");
+           System.out.println("1. Admin login");
+           System.out.println("2. Customer ");
+           System.out.println("0. Exit");
+           System.out.print("Choice: ");
            choice = scanner.nextInt();
             }
             switch (choice) {
@@ -34,55 +37,54 @@ public class Menu {
                     showCustomerMenu(scanner);
                     break;
                 case 0:
-                    System.out.println("Αποσυνδεθήκατε."); 
+                    System.out.println("You're offline.");
                     break;
             
                   
             }
         
     }
-      
 
     // Μενού Διαχειριστή
     private void showAdminMenu(Scanner scanner) {
-        System.out.println("=== Σύνδεση Διαχειριστή ===");
-        System.out.print("Όνομα χρήστη: ");
+        System.out.println("===Admin Connection===");
+        System.out.print("User name: ");
         String username = scanner.next();
-        System.out.print("Κωδικός πρόσβασης: ");
+        System.out.print("Password: ");
         String password = scanner.next();
 
         if (username.equals("admin") && password.equals("1234")) {
-            System.out.println("Επιτυχής σύνδεση!");
+            System.out.println("Connection succeed!");
             system.displayServices(); // Εμφάνιση υπηρεσιών για έλεγχο
         } else {
-            System.out.println("Λάθος όνομα χρήστη ή κωδικός πρόσβασης.");
+            System.out.println("Wrong username or password.Please try again.");
         }
     }
-
-    // Μενού Πελάτη
+     
     private void showCustomerMenu(Scanner scanner) {
-        System.out.println("=== Μενού Πελάτη ===");
+        System.out.println("=== Customer Menu ===");
         system.displayServices();
-        System.out.print("Επιλέξτε αριθμό υπηρεσίας: ");
+        System.out.print("Choose service number: ");
         int serviceChoice = scanner.nextInt();
         Scanner scanner1 = new Scanner(System.in);
-        System.out.println("Κρατήσεις Πελατών");
+        System.out.println("Customer Reservation");
 
-        System.out.print("Όνοματεπώνυμο: ");
+        System.out.print("Name : ");
         String name = scanner1.nextLine();
 
-        System.out.print("Διαθέσιμο εύρος ωρών (π.χ. 10-14): ");
+        System.out.print("Availability range (for example 10-14): ");
         
         String hours = scanner1.nextLine();
 
         Service selectedService = system.getServiceByIndex(serviceChoice - 1);
         if (selectedService != null) {
-            System.out.println("Επιλέξατε: " + selectedService.getName());
-            System.out.println("Κόστος: " + selectedService.getCost() + "€");
-            System.out.println("Διάρκεια: " + selectedService.getDuration() + " ώρες");
-            System.out.println("Ευχαριστούμε που μας επιλέξατε θα ενημερωθείτε αύριο για την ακριβή ώρα του ραντεβού σας");
+            System.out.println("You chose: " + selectedService.getName());
+            System.out.println("Cost: " + selectedService.getCost() + "€");
+            System.out.println("Duration: " + selectedService.getDuration() + " hours");
+            System.out.println("Thank you for choosing us you will be informed tommorow for the exact time of your reservation");
         } else {
-            System.out.println("Μη έγκυρη επιλογή.");
+            System.out.println("Not valid answer.");
         }
     }
 }
+   
