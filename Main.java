7bd00a8450ff5e688d2e1095d08e05320ba51e2f
elenaxenouu <t8220110@aueb.gr>
2024-11-26 18:23:00 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
 
 public class Main {
     public static void main(String[] args) {
-         AppointmentSystem system = new AppointmentSystem();
+        AppointmentSystem system = new AppointmentSystem();
 
         // Προσθήκη υπηρεσιών από τον διαχειριστή
-        system.addService(new Service("Υπηρεσια1", 15.0, 1));
-        system.addService(new Service("Υπηρεσια2", 50.0, 3));
-        system.addService(new Service("Υπηρεσια3", 20.0, 1));
-        system.addService(new Service("Υπηρεσια4", 35.0, 1));
-        system.addService(new Service("Υπηρεσια5", 10.0, 2));
-        system.addService(new Service("Υπηρεσια6", 20.0, 3));
-        system.addService(new Service("Υπηρεσια7", 32.0, 2));
-        Menu menu = new Menu(system);
-        menu.showMainMenu();
+        system.addService(new Service("Service 1", 15.0, 1));
+        system.addService(new Service("Service 2", 50.0, 3));
+        system.addService(new Service("Service 3", 20.0, 1));
+        system.addService(new Service("Service 4", 60.0, 2));
+        system.addService(new Service("Service 5", 70.0, 3));
+        system.addService(new Service("Service 6", 50.0, 2));
+        system.addService(new Service("Service 7", 80.0, 3));
         
+        
+     
+       Menu menu = new Menu(system);
+       menu.showMainMenu();
+                                                         
     }
 }
+
+
